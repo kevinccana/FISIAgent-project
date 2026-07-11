98fcb4fc9d12c024e653d4b64c688cf297d81283
jeison-cf <jason.ti@comprafacil-usa.com>
2026-07-11 11:27:54 -0500

    FEATURE: add doc for make cloud deploy

--- FISIAgent-Front/vite.config.js
+++ FISIAgent-Front/vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+  // GitHub Pages sirve el sitio en https://<usuario>.github.io/FISIAgent-project/,
+  // así que el build de producción necesita ese subpath. `npm run dev` sigue en '/'.
+  base: command === 'build' ? '/FISIAgent-project/' : '/',
   plugins: [react()],
-})
+}))
